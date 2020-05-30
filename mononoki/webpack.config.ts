176ad6306ef0296941c3583407226e525b19786d
Mizael Silva <sebastianplastbald@gmail.com>
2020-05-30 16:40:25 -0300

    Subindo font Mononoki.

---
+++ mononoki/webpack.config.ts
@@ -0,0 +1,64 @@
+const webpack = require('webpack')
+const path = require('path')
+const MiniCssExtract = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const OptimizeCSSAssetsPluginProd = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+  entry: './index.ts',
+  output: {
+    path: path.join(__dirname, 'fonts'),
+    filename: 'app.[hash].js',
+  },
+  plugins: [
+    new MiniCssExtract({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new OptimizeCSSAssetsPluginProd({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(styl)$/,
+        use: [
+          {
+            loader: MiniCssExtract.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          'css-loader',
+          'stylus-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtract.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
